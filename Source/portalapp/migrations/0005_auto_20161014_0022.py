945d99acf366165e06fed0f6c2e6e3a3ed9402f6
MaverickMe4556 <fireball4556@gmail.com>
2016-10-27 17:44:17 +0530

    first

---
+++ Source/portalapp/migrations/0005_auto_20161014_0022.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('portalapp', '0004_register'),
+    ]
+
+    operations = [
+        migrations.DeleteModel(
+            name='Event',
+        ),
+        migrations.DeleteModel(
+            name='Photo',
+        ),
+    ]
