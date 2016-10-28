b841dbce0569f9346953e4b719871cb1c3adcefa
BhumikaGoel <bhumikagoel@gmail.com>
2016-10-28 16:53:52 +0530

    bhu1

--- Source/portalapp/views.py
+++ Source/portalapp/views.py
@@ -27,11 +27,6 @@ def photo_list(request):
 """def talks_list(request):
     talks = Register.objects.filter(date_and_time__gte = timezone.now()).order_by('date_and_time')
       form = TalkForm(request.POST, request.FILES)
-      if form.is_valid():
-         talk = form.save(commit=False)
-         talk.author = request.user
-         talk.save()
-         return redirect('talks_list')
 
     else:
       form = TalkForm()
